2919de5dbd299ebc5160b1faaf3b9170013a028e
keingsw <dev.keingsw@gmail.com>
2021-07-26 10:43:11 +0900

    Setup ESLint and Prettier

---
+++ eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
+  ],
+  rules: {}
+};
