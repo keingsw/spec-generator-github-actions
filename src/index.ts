1646e7b827522b270fe90a50f2b100d9276d300d
keingsw <dev.keingsw@gmail.com>
2021-07-29 10:31:33 +0900

    Implement try catch

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,66 @@
 import path from "path";
-import glob from "glob";
 import * as core from "@actions/core";
+import { updateToc } from "./toc";
+import { generatePdf } from "./generate-pdf";
+import { updateRevisionHistory } from "./revision-history";
 import {
     getPullRequestByBranchName,
     commitChangesToBranch,
 } from "./utils/github";
-import { updateToc } from "./toc";
-import { generatePdf } from "./generate-pdf";
-import { updateRevisionHistory } from "./revision-history";
 import { findMarkdownFiles } from "./utils/fs";
 
 async function run() {
-    const specDir = core.getInput("specDir");
-    const chapterContentsFilename = core.getInput("chapterContentsFilename");
-    const chapterIndexFilename = core.getInput("chapterIndexFilename");
-    const outputDir = core.getInput("outputDir");
-    const outputFilename = core.getInput("outputFilename");
-
-    const [branchName] = core.getInput("branchRef").split("/").slice(-1);
-    const { number: prNumber } = await getPullRequestByBranchName(branchName);
-
     const author = {
         name: "github-actions",
         email: "github-actions@github.com",
     };
 
-    updateToc({
-        specDir,
-        chapterIndexFilename,
-        chapterContentsFilename,
-    });
-    await updateRevisionHistory({
-        prNumber,
-        specDir,
-        chapterIndexFilename,
-    });
-    await generatePdf({
-        specDir,
-        chapterContentsFilename,
-        outputDir,
-        outputFilename,
-    });
+    try {
+        const specDir = core.getInput("specDir");
+        const chapterContentsFilename = core.getInput(
+            "chapterContentsFilename"
+        );
+        const chapterIndexFilename = core.getInput("chapterIndexFilename");
+        const outputDir = core.getInput("outputDir");
+        const outputFilename = core.getInput("outputFilename");
 
-    await commitChangesToBranch({
-        branchName,
-        files: [
-            ...findMarkdownFiles(specDir).map((filePath) =>
-                path.relative("./", filePath)
-            ),
-            path.relative("./", `${outputDir}/${outputFilename}`),
-        ],
-        author,
-        commitMessage: "Update TOC and revision history, and re-generate PDF",
-    });
+        const [branchName] = core.getInput("branchRef").split("/").slice(-1);
+        const { number: prNumber } = await getPullRequestByBranchName(
+            branchName
+        );
+
+        updateToc({
+            specDir,
+            chapterIndexFilename,
+            chapterContentsFilename,
+        });
+        await updateRevisionHistory({
+            prNumber,
+            specDir,
+            chapterIndexFilename,
+        });
+        await generatePdf({
+            specDir,
+            chapterContentsFilename,
+            outputDir,
+            outputFilename,
+        });
+
+        await commitChangesToBranch({
+            branchName,
+            files: [
+                ...findMarkdownFiles(specDir).map((filePath) =>
+                    path.relative("./", filePath)
+                ),
+                path.relative("./", `${outputDir}/${outputFilename}`),
+            ],
+            author,
+            commitMessage:
+                "Update TOC and revision history, and re-generate PDF",
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
